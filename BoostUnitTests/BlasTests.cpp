2829478e25c3261edb7c44f41c3bce6101058027
PAVLOS G. GALIATSATOS <paulsepolia@gmail.com>
2020-12-16 12:44:59 +0000

    Added test cases for 'dgemv' using 'Vector' container

--- BoostUnitTests/BlasTests.cpp
+++ BoostUnitTests/BlasTests.cpp
@@ -1,21 +1,36 @@
 #define BOOST_TEST_DYN_LINK
 
+#include "../Containers/Vector/Vector.h"
 #include <boost/test/unit_test.hpp>
 #include <cblas.h>
+
+using namespace SEPOLIA4::CONTAINERS;
 
 namespace SEPOLIA4::BOOST_UNIT_TEST_BLAS
 {
 	BOOST_AUTO_TEST_SUITE(BLAS_TESTS)
 
-		BOOST_AUTO_TEST_CASE(TEST1)
+		BOOST_AUTO_TEST_CASE(TEST_CBLAS_DGEMV_aAX_1)
 		{
 			const int32_t NROWS = 4;
 			const int32_t NCOLS = 5;
+			const size_t TOT_ELEMS = NROWS * NCOLS;
 
-			double A[] = { 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4 };
-			double x[] = { 1, 1, 1, 1, 1 };
+			double A[TOT_ELEMS];
+			double x[NCOLS];
+
+			for (size_t i = 0; i < TOT_ELEMS; i++)
+			{
+				A[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NCOLS; i++)
+			{
+				x[i] = static_cast<double>(i + 1);
+			}
+
 			double y[NROWS];
-			const double alpha = 1.0;
+			const double alpha = 1.234;
 			const double beta = 0.0;
 
 			//========//
@@ -23,21 +38,273 @@ namespace SEPOLIA4::BOOST_UNIT_TEST_BLAS
 			//========//
 			cblas_dgemv(CblasRowMajor, CblasNoTrans, NROWS, NCOLS, alpha, A, NCOLS, x, 1, beta, y, 1);
 
-			std::vector<double> resExpected1{ 5.0, 10.0, 15.0, 20.0 };
+			const std::vector<double> resExpected{ alpha * 55, alpha * 130, alpha * 205, alpha * 280 };
+
 			for (int i = 0; i < NROWS; i++)
 			{
-				BOOST_TEST(resExpected1[i] == y[i]);
+				BOOST_TEST(resExpected[i] == y[i]);
 			}
+		}
+
+		BOOST_AUTO_TEST_CASE(TEST_CBLAS_DGEMV_AX_2)
+		{
+			const int32_t NROWS = 1;
+			const int32_t NCOLS = 5;
+			const size_t TOT_ELEMS = NROWS * NCOLS;
+
+			double A[TOT_ELEMS];
+			double x[NCOLS];
+
+			for (size_t i = 0; i < TOT_ELEMS; i++)
+			{
+				A[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NCOLS; i++)
+			{
+				x[i] = static_cast<double>(i + 1);
+			}
+
+			double y[NROWS];
+			const double alpha = 1.234;
+			const double beta = 0.0;
 
 			//========//
 			// y = Ax //
 			//========//
-			cblas_dgemv(CblasColMajor, CblasNoTrans, NROWS, NCOLS, alpha, A, NROWS, x, 1, beta, y, 1);
+			cblas_dgemv(CblasRowMajor, CblasNoTrans, NROWS, NCOLS, alpha, A, NCOLS, x, 1, beta, y, 1);
 
-			std::vector<double> resExpected2{ 11.0, 12.0, 13.0, 14.0 };
+			const std::vector<double> resExpected{ alpha * 55 };
+
 			for (int i = 0; i < NROWS; i++)
 			{
-				BOOST_TEST(resExpected2[i] == y[i]);
+				BOOST_TEST(resExpected[i] == y[i]);
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(TEST_CBLAS_DGEMV_AX_3)
+		{
+			const int32_t NROWS = 5;
+			const int32_t NCOLS = 1;
+			const size_t TOT_ELEMS = NROWS * NCOLS;
+
+			double A[TOT_ELEMS];
+			double x[NCOLS];
+
+			for (size_t i = 0; i < TOT_ELEMS; i++)
+			{
+				A[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NCOLS; i++)
+			{
+				x[i] = static_cast<double>(i + 1);
+			}
+
+			double y[NROWS];
+			const double alpha = 1.2345;
+			const double beta = 0.0;
+
+			//========//
+			// y = Ax //
+			//========//
+			cblas_dgemv(CblasRowMajor, CblasNoTrans, NROWS, NCOLS, alpha, A, NCOLS, x, 1, beta, y, 1);
+
+			const std::vector<double> resExpected{ 1 * alpha, 2 * alpha, 3 * alpha, 4 * alpha, 5 * alpha };
+
+			for (int i = 0; i < NROWS; i++)
+			{
+				BOOST_TEST(resExpected[i] == y[i]);
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(TEST_CBLAS_DGEMV_aAX_plus_bY)
+		{
+			const int32_t NROWS = 4;
+			const int32_t NCOLS = 5;
+			const size_t TOT_ELEMS = NROWS * NCOLS;
+
+			double A[TOT_ELEMS];
+			double x[NCOLS];
+			double y[NROWS];
+
+			for (size_t i = 0; i < TOT_ELEMS; i++)
+			{
+				A[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NCOLS; i++)
+			{
+				x[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NROWS; i++)
+			{
+				y[i] = static_cast<double>(i);
+			}
+
+			const double alpha = 1.234;
+			const double beta = 2.345;
+			const std::vector<double> resExpected{ alpha * 55 + beta * y[0], alpha * 130 + beta * y[1],
+												   alpha * 205 + beta * y[2], alpha * 280 + beta * y[3] };
+
+			//========//
+			// y = Ax //
+			//========//
+			cblas_dgemv(CblasRowMajor, CblasNoTrans, NROWS, NCOLS, alpha, A, NCOLS, x, 1, beta, y, 1);
+
+			for (int i = 0; i < NROWS; i++)
+			{
+				BOOST_TEST(resExpected[i] == y[i]);
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(TEST_CBLAS_DGEMV_aAX_1_USING_VECTOR)
+		{
+			const int32_t NROWS = 4;
+			const int32_t NCOLS = 5;
+			const size_t TOT_ELEMS = NROWS * NCOLS;
+
+			Vector A(TOT_ELEMS);
+			Vector x(NCOLS);
+
+			for (size_t i = 0; i < TOT_ELEMS; i++)
+			{
+				A[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NCOLS; i++)
+			{
+				x[i] = static_cast<double>(i + 1);
+			}
+
+			Vector y(NROWS);
+			const double alpha = 1.234;
+			const double beta = 0.0;
+
+			//========//
+			// y = Ax //
+			//========//
+			cblas_dgemv(CblasRowMajor, CblasNoTrans, NROWS, NCOLS, alpha, &A[0], NCOLS, &x[0], 1, beta, &y[0], 1);
+
+			const Vector resExpected({ alpha * 55, alpha * 130, alpha * 205, alpha * 280 });
+
+			for (int i = 0; i < NROWS; i++)
+			{
+				BOOST_TEST(resExpected.At(i) == y[i]);
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(TEST_CBLAS_DGEMV_AX_2_USING_VECTOR)
+		{
+			const int32_t NROWS = 1;
+			const int32_t NCOLS = 5;
+			const size_t TOT_ELEMS = NROWS * NCOLS;
+
+			Vector A(TOT_ELEMS);
+			Vector x(NCOLS);
+
+			for (size_t i = 0; i < TOT_ELEMS; i++)
+			{
+				A[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NCOLS; i++)
+			{
+				x[i] = static_cast<double>(i + 1);
+			}
+
+			Vector y(NROWS);
+			const double alpha = 1.234;
+			const double beta = 0.0;
+
+			//========//
+			// y = Ax //
+			//========//
+			cblas_dgemv(CblasRowMajor, CblasNoTrans, NROWS, NCOLS, alpha, &A[0], NCOLS, &x[0], 1, beta, &y[0], 1);
+
+			const Vector resExpected({ alpha * 55 });
+
+			for (int i = 0; i < NROWS; i++)
+			{
+				BOOST_TEST(resExpected.At(i) == y[i]);
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(TEST_CBLAS_DGEMV_AX_3_USING_VECTOR)
+		{
+			const int32_t NROWS = 5;
+			const int32_t NCOLS = 1;
+			const size_t TOT_ELEMS = NROWS * NCOLS;
+
+			Vector A(TOT_ELEMS);
+			Vector x(NCOLS);
+
+			for (size_t i = 0; i < TOT_ELEMS; i++)
+			{
+				A[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NCOLS; i++)
+			{
+				x[i] = static_cast<double>(i + 1);
+			}
+
+			Vector y(NROWS);
+			const double alpha = 1.2345;
+			const double beta = 0.0;
+
+			//========//
+			// y = Ax //
+			//========//
+			cblas_dgemv(CblasRowMajor, CblasNoTrans, NROWS, NCOLS, alpha, &A[0], NCOLS, &x[0], 1, beta, &y[0], 1);
+
+			const Vector resExpected({ 1 * alpha, 2 * alpha, 3 * alpha, 4 * alpha, 5 * alpha });
+
+			for (int i = 0; i < NROWS; i++)
+			{
+				BOOST_TEST(resExpected.At(i) == y[i]);
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(TEST_CBLAS_DGEMV_aAX_plus_bY_USING_VECTOR)
+		{
+			const int32_t NROWS = 4;
+			const int32_t NCOLS = 5;
+			const size_t TOT_ELEMS = NROWS * NCOLS;
+
+			Vector A(TOT_ELEMS);
+			Vector x(NCOLS);
+			Vector y(NROWS);
+
+			for (size_t i = 0; i < TOT_ELEMS; i++)
+			{
+				A[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NCOLS; i++)
+			{
+				x[i] = static_cast<double>(i + 1);
+			}
+
+			for (size_t i = 0; i < NROWS; i++)
+			{
+				y[i] = static_cast<double>(i);
+			}
+
+			const double alpha = 1.234;
+			const double beta = 2.345;
+			const Vector resExpected({ alpha * 55 + beta * y[0], alpha * 130 + beta * y[1],
+									   alpha * 205 + beta * y[2], alpha * 280 + beta * y[3] });
+
+			//========//
+			// y = Ax //
+			//========//
+			cblas_dgemv(CblasRowMajor, CblasNoTrans, NROWS, NCOLS, alpha, &A[0], NCOLS, &x[0], 1, beta, &y[0], 1);
+
+			for (int i = 0; i < NROWS; i++)
+			{
+				BOOST_TEST(resExpected.At(i) == y[i]);
 			}
 		}
 
